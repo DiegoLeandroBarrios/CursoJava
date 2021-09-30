e298b661ca35ff0969d83314ed6b1b696fac4189
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2021-09-30 00:40:40 -0300

    desafio 6

---
+++ src/DivporZero.java
@@ -0,0 +1,6 @@
+public class DivporZero {
+    public static void main(String[] args) {
+        double combinado =(((6.0/5.0) - (2.0/3.0) * (7.0/2.0) +(2.0/30.0))/(((1.0/3.0)/5.0)));
+        System.out.println(combinado);
+    }
+}
